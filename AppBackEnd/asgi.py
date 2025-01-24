7fa486777ee9b764042f8678a0444c7bd8376c5c
Josh L <joshua.levine42@gmail.com>
2025-01-24 18:38:01 -0500

    Initial Commit

---
+++ AppBackEnd/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for AppBackEnd project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+# Asgi is for asynchronous python to intereact w/ socket io
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppBackEnd.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            # Define WebSocket routing here
+        )
+    ),
+})
