5849c74d1792db2820f2a9327309e7c9f4fbc4b3
Josh L <joshua.levine42@gmail.com>
2025-01-26 01:30:53 -0500

    fixed missing comma

--- AppBackEnd/settings.py
+++ AppBackEnd/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
     ),
 }
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
