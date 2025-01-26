f872a6370c94768fc61885503afa411cb62479ef
Josh L <joshua.levine42@gmail.com>
2025-01-26 01:17:13 -0500

    added cors

--- AppBackEnd/settings.py
+++ AppBackEnd/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'users',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
     ),
 }
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
 ]
 
 ROOT_URLCONF = 'AppBackEnd.urls'
